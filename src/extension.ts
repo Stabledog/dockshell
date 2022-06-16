6b95eae0b83bec0edc518738c6da998c1cd1e81d
Les Matheson <les.matheson@gmail.com>
2022-06-15 22:23:27 -0400

    Sync (auto)

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "dockshell" is now active!');
@@ -17,6 +17,8 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from DockShell!');
+        vscode.commands.getCommands().then((v) => {console.log(v); });
+        console.log("End of command handler for dockshell.helloWorld");
 	});
 
 	context.subscriptions.push(disposable);
